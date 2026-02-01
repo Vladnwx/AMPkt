cc274bdf5e632b150103715f6fa89bdd7628cbdc
Vladimir <anykey196@gmail.com>
2026-02-01 14:33:27 +0500

    рефакторинг(data): обновить NominalSizeDao под нормализованную схему
    - Использовать имя таблицы nominal_size и колонку size_mm2
    - Обновить запрос getAllListDouble() для выборки только size_mm2
    - Исправить сортировку на числовую (по size_mm2)
    - Добавить id при вставке стандартных значений
    - Сделать insertDefaultValues() suspend-функцией

--- app/src/main/java/com/amp/data/dao/NominalSizeDao.kt
+++ app/src/main/java/com/amp/data/dao/NominalSizeDao.kt
@@ -1,103 +1,53 @@
 package com.amp.data.dao
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Query
 import com.amp.data.entity.NominalSize
 import kotlinx.coroutines.flow.Flow
 
 @Dao
-interface NominalSizeDao :BaseDAO<NominalSize> {
-    @Query("SELECT * FROM nominalSize")
+interface NominalSizeDao : BaseDAO<NominalSize> {
+
+    @Query("SELECT * FROM nominal_size")
     fun getAll(): List<NominalSize>
 
-    @Query("SELECT * FROM nominalSize")
+    @Query("SELECT * FROM nominal_size")
     fun getAllFlow(): Flow<List<NominalSize>>
 
-    @Query("SELECT * FROM nominalSize")
-    fun getAllListDouble(): MutableList<Double>
-    @Query("SELECT * FROM nominalSize ORDER BY value ASC")
+    @Query("SELECT size_mm2 FROM nominal_size")
+    fun getAllListDouble(): List<Double>
+
+    @Query("SELECT * FROM nominal_size ORDER BY size_mm2 ASC")
     fun getAlphabetized(): Flow<List<NominalSize>>
 
-    @Query("DELETE FROM nominalSize")
+    @Query("DELETE FROM nominal_size")
     suspend fun deleteAll()
 
-    @Query(
-        """INSERT INTO nominalSize (
-                             value
-                         )
-                         VALUES (
-                             0.5
-                         ),
-                         (
-                             0.75
-                         ),
-                         (
-                             1.5
-                         ),
-                         (
-                             2.5
-                         ),
-                         (
-                             4
-                         ),
-                         (
-                             6
-                         ),
-                         (
-                             10
-                         ),
-                         (
-                             16
-                         ),
-                         (
-                             25
-                         ),
-                         (
-                             35
-                         ),
-                         (
-                             50
-                         ),
-                         (
-                             70
-                         ),
-                         (
-                             95
-                         ),
-                         (
-                             120
-                         ),
-                         (
-                             150
-                         ),
-                         (
-                             185
-                         ),
-                         (
-                             240
-                         ),
-                         (
-                             300
-                         ),
-                         (
-                             400
-                         ),
-                         (
-                             500
-                         ),
-                         (
-                             630
-                         ),
-                         (
-                             800
-                         ),
-                         (
-                             1000
-                         );
-"""
-    )
-    fun defaultgreate()
-
-
+    @Query("""
+        INSERT INTO nominal_size (id, size_mm2) VALUES
+        (1, 0.5),
+        (2, 0.75),
+        (3, 1.5),
+        (4, 2.5),
+        (5, 4.0),
+        (6, 6.0),
+        (7, 10.0),
+        (8, 16.0),
+        (9, 25.0),
+        (10, 35.0),
+        (11, 50.0),
+        (12, 70.0),
+        (13, 95.0),
+        (14, 120.0),
+        (15, 150.0),
+        (16, 185.0),
+        (17, 240.0),
+        (18, 300.0),
+        (19, 400.0),
+        (20, 500.0),
+        (21, 630.0),
+        (22, 800.0),
+        (23, 1000.0)
+    """)
+    suspend fun insertDefaultValues()
 }
