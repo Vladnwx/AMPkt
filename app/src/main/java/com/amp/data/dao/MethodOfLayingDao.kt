5599b771000520ef352a839e54d014a25596bb14
Vladimir <anykey196@gmail.com>
2026-02-01 14:35:34 +0500

    рефакторинг(data): обновить MethodOfLayingDao с корректными терминами
    - Уточнить значения: "Одиночная прокладка" и "Прокладка в пучке"
    - Соответствует техническому смыслу (влияние взаимного нагрева)
    - Использовать snake_case имя таблицы и колонку name
    - Добавить id при вставке, сделать метод suspend

--- app/src/main/java/com/amp/data/dao/MethodOfLayingDao.kt
+++ app/src/main/java/com/amp/data/dao/MethodOfLayingDao.kt
@@ -5,35 +5,25 @@ import androidx.room.Query
 import com.amp.data.entity.MethodOfLaying
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
-interface MethodOfLayingDao:BaseDAO<MethodOfLaying>{
+interface MethodOfLayingDao : BaseDAO<MethodOfLaying> {
 
-    @Query("SELECT * FROM MethodOfLaying")
-    fun getAll(): List<String>
+    @Query("SELECT * FROM method_of_laying")
+    fun getAll(): List<MethodOfLaying>
 
-    @Query("SELECT * FROM MethodOfLaying")
+    @Query("SELECT * FROM method_of_laying")
     fun getAllFlow(): Flow<List<MethodOfLaying>>
 
-
-    @Query("SELECT * FROM MethodOfLaying ORDER BY value ASC")
+    @Query("SELECT * FROM method_of_laying ORDER BY name ASC")
     fun getAlphabetized(): Flow<List<MethodOfLaying>>
 
-    @Query("DELETE FROM MethodOfLaying")
+    @Query("DELETE FROM method_of_laying")
     suspend fun deleteAll()
 
-    @Query(
-        """INSERT INTO MethodOfLaying (
-                                 value
-                             )
-                             VALUES (
-                                 'single laying'
-                             ),
-                             (
-                                 'bundle laying'
-                             );
-"""
-    )
-    fun defaultgreate()
-
+    @Query("""
+        INSERT INTO method_of_laying (id, name) VALUES
+        (1, 'Одиночная прокладка'),
+        (2, 'Прокладка в пучке')
+    """)
+    suspend fun insertDefaultValues()
 }
