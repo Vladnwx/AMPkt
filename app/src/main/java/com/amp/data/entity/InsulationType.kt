9ce8d1b5a6abc4438b292bdd0f0027f76915fb3c
Vladimir <anykey196@gmail.com>
2026-02-01 14:22:25 +0500

    рефакторинг(data): нормализовать сущность InsulationType
    - Заменить строковой первичный ключ на целочисленный id
    - Добавить поле name для хранения названия типа изоляции
    - Привести имя таблицы к snake_case: insulation_type

--- app/src/main/java/com/amp/data/entity/InsulationType.kt
+++ app/src/main/java/com/amp/data/entity/InsulationType.kt
@@ -1,21 +1,15 @@
 package com.amp.data.entity
 
-import androidx.annotation.NonNull
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-
-@Entity(tableName = "insulationType")
-
-data class InsulationType (
-
-    @PrimaryKey
-    @ColumnInfo(name = "value")
-    val value: String) {
-
-    override fun toString(): String {
-        return value
-    }
-
-}
+/**
+ * Справочник: тип изоляции кабеля
+ * Примеры: "ПВХ", "XLPE", "Резина", "Бумажная"
+ */
+@Entity(tableName = "insulation_type")
+data class InsulationType(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "name") val name: String
+)
