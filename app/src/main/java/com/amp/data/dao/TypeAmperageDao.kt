573c4a3d50794d86008ba134f990177b2e743ab1
Vladimir <anykey196@gmail.com>
2026-02-01 14:31:42 +0500

    рефакторинг(data): обновить TypeAmperageDao под нормализованную схему
    - Использовать имя таблицы type_amperage
    - Заменить колонку value на name
    - Возвращать объекты вместо List<String>
    - Обновить запрос вставки значений с id и name
    - Сделать insertDefaultValues() suspend-функцией

--- app/src/main/java/com/amp/data/dao/TypeAmperageDao.kt
+++ app/src/main/java/com/amp/data/dao/TypeAmperageDao.kt
@@ -5,34 +5,25 @@ import androidx.room.Query
 import com.amp.data.entity.TypeAmperage
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
-interface TypeAmperageDao:BaseDAO<TypeAmperage>{
+interface TypeAmperageDao : BaseDAO<TypeAmperage> {
 
-    @Query("SELECT * FROM TypeAmperage")
-    fun getAll(): List<String>
+    @Query("SELECT * FROM type_amperage")
+    fun getAll(): List<TypeAmperage>
 
-    @Query("SELECT * FROM TypeAmperage")
+    @Query("SELECT * FROM type_amperage")
     fun getAllFlow(): Flow<List<TypeAmperage>>
 
-
-    @Query("SELECT * FROM TypeAmperage ORDER BY value ASC")
+    @Query("SELECT * FROM type_amperage ORDER BY name ASC")
     fun getAlphabetized(): Flow<List<TypeAmperage>>
 
-    @Query("DELETE FROM TypeAmperage")
+    @Query("DELETE FROM type_amperage")
     suspend fun deleteAll()
 
-    @Query(
-        """INSERT INTO TypeAmperage (
-                              value
-                          )
-                          VALUES (
-                              'AC'
-                          ),
-                          (
-                              'DC'
-                          );"""
-    )
-    fun defaultgreate()
-
+    @Query("""
+        INSERT INTO type_amperage (id, name) VALUES
+        (1, 'Переменный ток'),
+        (2, 'Постоянный ток')
+    """)
+    suspend fun insertDefaultValues()
 }
