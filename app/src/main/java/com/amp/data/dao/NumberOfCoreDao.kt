5935e8b9a6640ee5d4d56dd42d80ef862fb67ee9
Vladimir <anykey196@gmail.com>
2026-02-01 14:32:35 +0500

    рефакторинг(data): обновить NumberOfCoreDao под нормализованную схему
    - Использовать имя таблицы number_of_core
    - Заменить колонку value на name
    - Возвращать объекты вместо List<String>
    - Обновить стандартные значения на понятные пользователю (1, 3+1, 5 и т.д.)
    - Добавить id при вставке, сделать метод suspend

--- app/src/main/java/com/amp/data/dao/NumberOfCoreDao.kt
+++ app/src/main/java/com/amp/data/dao/NumberOfCoreDao.kt
@@ -5,37 +5,29 @@ import androidx.room.Query
 import com.amp.data.entity.NumberOfCore
 import kotlinx.coroutines.flow.Flow
 
-
 @Dao
-interface NumberOfCoreDao:BaseDAO<NumberOfCore>{
+interface NumberOfCoreDao : BaseDAO<NumberOfCore> {
 
-    @Query("SELECT * FROM NumberOfCore")
-    fun getAll(): List<String>
+    @Query("SELECT * FROM number_of_core")
+    fun getAll(): List<NumberOfCore>
 
-    @Query("SELECT * FROM NumberOfCore")
+    @Query("SELECT * FROM number_of_core")
     fun getAllFlow(): Flow<List<NumberOfCore>>
 
-
-    @Query("SELECT * FROM NumberOfCore ORDER BY value ASC")
+    @Query("SELECT * FROM number_of_core ORDER BY name ASC")
     fun getAlphabetized(): Flow<List<NumberOfCore>>
 
-    @Query("DELETE FROM NumberOfCore")
+    @Query("DELETE FROM number_of_core")
     suspend fun deleteAll()
 
-    @Query(
-        """INSERT INTO NumberOfCore (
-                                value
-                            )
-                            VALUES (
-                                'single'
-                            ),
-                            (
-                                'multicore5'
-                            ),
-                            (
-                                'multicore3'
-                            );"""
-    )
-    fun defaultgreate()
-
+    @Query("""
+        INSERT INTO number_of_core (id, name) VALUES
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '3+1'),
+        (5, '4'),
+        (6, '5')
+    """)
+    suspend fun insertDefaultValues()
 }
